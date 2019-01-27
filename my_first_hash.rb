6f2a1a5a3e49cd6cb3cdf555b2c702c080a841d2
Marci Clark <marcig612@gmail.com>
2019-01-27 03:06:37 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -28,7 +28,8 @@ def adding
                   "oil paintings" => 3
                  }
   
-  shipping_manifest["muskets"]
+  shipping_manifest["muskets"] = 2
+  shipping_manifest["gun powder"] = 4
 
 
 
